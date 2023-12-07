7ec52350ecf9200c307a89c6b38948ad25954158
Young <yoojinyoung@kakao.com>
2023-12-07 19:02:03 +0900

    feat: 홈페이지의 포스트 목록 동적 구현

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import dataAPI from "../../api/data.api";
 import Page from "../../components/Page";
+import { authActions } from "../../redux/slices/auth.slice";
 
 function HomePage() {
   const [posts, setPosts] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dataAPI
@@ -11,10 +15,22 @@ function HomePage() {
       .then((res) => setPosts(res.data))
       .catch((e) => {
         console.log(e.config.signal.reason);
+        dispatch(authActions.logOut());
       });
   }, []);
 
-  return <Page title="홈"></Page>;
+  return (
+    <Page title="홈">
+      <h2>포스트 목록</h2>
+      <ol>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={"/"}>{post.title}</Link>
+          </li>
+        ))}
+      </ol>
+    </Page>
+  );
 }
 
 export default HomePage;
