cd13ffd9889841990873faeaee59b086ea4160ba
Young <yoojinyoung@kakao.com>
2023-12-07 15:47:30 +0900

    feat: Header에 Controls 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 function Header() {
   return (
     <Wrapper>
-      <Logo>인증 연습하기</Logo>
+      <Logo to="/">인증 연습하기</Logo>
+      <Controls>
+        <Button to={"/login"}>로그인하기</Button>
+        <Button to={"/signup"}>회원가입하기</Button>
+      </Controls>
     </Wrapper>
   );
 }
@@ -18,9 +23,34 @@ const Wrapper = styled.header`
   padding: 0 1rem;
 `;
 
-const Logo = styled.strong`
+const Logo = styled(Link)`
   font-size: 1.5rem;
   font-weight: 700;
+  text-decoration: none;
+  &:visited {
+    color: black;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+  column-gap: 1rem;
+`;
+
+const Button = styled(Link)`
+  background-color: white;
+  border: 1px solid black;
+  padding: 0.5rem 1.5rem;
+  font-size: 15px;
+  font-weight: 500;
+  text-decoration: none;
+  &:visited {
+    color: black;
+  }
+  &:hover {
+    background-color: #ccc;
+  }
 `;
 
 export default Header;
