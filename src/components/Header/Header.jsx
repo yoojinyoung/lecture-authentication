73266ff55bd51532d6aa6f4ee15c91f74e62e2e8
Young <yoojinyoung@kakao.com>
2023-12-07 16:02:03 +0900

    feat: `Header`의 `Controls`를 `isLoggedIn` 값에 따라 분기

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { logOut } from "../../redux/slices/auth.slice";
 
 function Header() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  const handleClickLogOut = () => {
+    dispatch(logOut());
+  };
+
   return (
     <Wrapper>
       <Logo to="/">인증 연습하기</Logo>
       <Controls>
-        <Button to={"/log-in"}>로그인하기</Button>
-        <Button to={"/sign-up"}>회원가입하기</Button>
+        {isLoggedIn ? (
+          <>
+            <Button onClick={handleClickLogOut}>로그아웃</Button>
+          </>
+        ) : (
+          <>
+            <LinkButton to={"/log-in"}>로그인</LinkButton>
+            <LinkButton to={"/sign-up"}>회원가입</LinkButton>
+          </>
+        )}
       </Controls>
     </Wrapper>
   );
@@ -38,7 +55,19 @@ const Controls = styled.div`
   column-gap: 1rem;
 `;
 
-const Button = styled(Link)`
+const Button = styled.button`
+  background-color: white;
+  border: 1px solid black;
+  padding: 0.5rem 1.5rem;
+  font-size: 15px;
+  font-weight: 500;
+  cursor: pointer;
+  &:hover {
+    background-color: #ccc;
+  }
+`;
+
+const LinkButton = styled(Link)`
   background-color: white;
   border: 1px solid black;
   padding: 0.5rem 1.5rem;
